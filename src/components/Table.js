42b8376da5c1b9cb83195bdda85e58268cc9aef1
dragon946470 <36012538+dragon946470@users.noreply.github.com>
2020-12-18 23:41:21 -0400

    Add files via upload
    update

--- src/components/Table.js
+++ src/components/Table.js
@@ -36,11 +36,11 @@ const Table = (props) => {
    console.log("propsTable", props);
     {/*TODO - return <table> component, TableHeader and TableBody  */}
 return(
-    <div>
+    <table>
        <TableHeader/>
       <TableBody linkData={linkData} removeLink={removeLink}/> 
      
-    </div>
+    </table>
 )
 
 }
